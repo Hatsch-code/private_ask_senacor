ff331bbcf19bd8a4eaa971953acd8a5cbf025f1f
richard-mueller <github@richardmueller.net>
2023-12-13 14:31:24 +0100

    use Azure OpenAI service instead of OpenAI API

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import os
 import streamlit as st
 from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
+#from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import AzureChatOpenAI
+
 from langchain.document_loaders import WebBaseLoader
-from langchain.embeddings import OpenAIEmbeddings
+#from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import AzureOpenAIEmbeddings
+
 from langchain.prompts.chat import (ChatPromptTemplate,
                                     HumanMessagePromptTemplate,
                                     SystemMessagePromptTemplate)
@@ -17,7 +21,12 @@ from streamlit_chat import message
 # Load environment variables from .env file (Optional)
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
+AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
+
+
+
 vector_store_address: str = os.getenv("YOUR_AZURE_SEARCH_ENDPOINT")
 vector_store_password: str = os.getenv("YOUR_AZURE_SEARCH_ADMIN_KEY")
 
@@ -75,13 +84,23 @@ def print_semantic_similarity(question, vector_store, k=3, search_type="similari
 # Simple QA
 def standard_query(question, k=3, model_name="gpt-3.5-turbo"):
     # Create OpenAI embeddings
-    openai_embeddings = OpenAIEmbeddings()
+    #openai_embeddings = OpenAIEmbeddings()
+    openai_embeddings = AzureOpenAIEmbeddings(
+        azure_deployment="AskSenacor-ada002-v1",
+        openai_api_version="2023-05-15",
+    )
     # Init Vector Store
     vector_store = init_vector_store(openai_embeddings)
+
     # Create a retriever from the Chroma vector database
     retriever = vector_store.as_retriever(search_kwargs={"k": k})
     # Use a ChatOpenAI model
-    llm = ChatOpenAI(model_name=model_name)
+    #llm = ChatOpenAI(model_name=model_name)
+    llm = AzureChatOpenAI(
+        azure_deployment="AskSenacor-gpt35turbo-v1",
+        openai_api_version="2023-05-15",
+    )
+
     # Create a RetrievalQA from the model and retriever
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
     return qa(question)
